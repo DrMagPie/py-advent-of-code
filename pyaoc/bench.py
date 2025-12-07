097140eb15e781658390499bbca0c636c554018b
DrMagPie <23652900+DrMagPie@users.noreply.github.com>
2025-12-07 13:21:00 +0200

    feat: additional controles for bench func

--- pyaoc/bench.py
+++ pyaoc/bench.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+from tqdm import trange
 
 from time import perf_counter_ns
 
@@ -7,12 +8,15 @@ from .get_session import get_session
 from .utils import format_time
 
 
-def bench(year: int, day: int, session_name: str = None) -> any:
+def bench(year: int, day: int, times: int = 100, progress: bool = False, print_all_runs: bool = False, session_name: str = None) -> any:
   """Wrapper function to get input and benchmark solver function
 
   Args:
       year (int): year of event
       day (int): day of year
+      times (int): number of times function will be measured
+      progress (bool): print a progress bar
+      print_all_runs (bool): prints results array with all run times
       session_name (str, optional): Name of session token to use, if not provided will prompt to input. Defaults to None.
 
   Returns:
@@ -23,9 +27,11 @@ def bench(year: int, day: int, session_name: str = None) -> any:
 
   def decorator(function):
     input_data, _ = get_input(year, day, session)
-    if not input_data: return function
+    if not input_data:
+      return function
     results = []
-    for i in range(10):
+    runs = trange(times) if progress else range(times)
+    for _ in runs:
       start = perf_counter_ns()
       function(input_data)
       end = perf_counter_ns()
@@ -35,7 +41,11 @@ def bench(year: int, day: int, session_name: str = None) -> any:
     minimum = min(results)
     maximum = max(results)
 
-    print(f'Average: {format_time(average)}\nMinimum: {format_time(minimum)}\nMaximum: {format_time(maximum)}\nResults: {results}')
+    message = f"Average: {format_time(average)}\nMinimum: {format_time(minimum)}\nMaximum: {format_time(maximum)}"
+
+    print(message)
+    if print_all_runs:
+      print(f"Results: {results}")
     return function
 
   return decorator
